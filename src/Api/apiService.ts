cf8b1d60a63cf069421508907c68772d23953d55
fred-18 <frednobre18@gmail.com>
2025-03-08 17:21:52 +0100

    add Api Service

--- src/Api/apiService.ts
+++ src/Api/apiService.ts
@@ -1,19 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Players } from '../Entities/player.entity';
+import { Repository } from 'typeorm';
+import * as process from 'node:process';
+import { PlayerDto } from '../playersDto.ts/PlayerDto';
 
 @Injectable()
 export class ApiService {
-  async getPlayers() {
-    const response = await axios.get(
-      'https://www.balldontlie.io/api/v1/players',
-    );
-    return response.data;
-  }
+  constructor(
+    @InjectRepository(Players)
+    private readonly playerRepository: Repository<Players>,
+  ) {}
 
-  async getTeams() {
-    const response = await axios.get('https://www.balldontlie.io/api/v1/teams');
-    console.log('testgit');
-    
-    return response.data;
-  } 
+  async fetchPlayerAndStore(
+    teamsId: number,
+    seasons: string,
+  ): Promise<Players> {
+    try {
+      const apiKey = process.env.API_KEY;
+      const apiHost = process.env.API_HOST;
+      const playersUrl = process.env.URL_PLAYER;
+
+      if (!apiKey || !apiHost || !playersUrl) {
+        throw new Error('API_KEY or API_HOST or URL_PLAYERS not found in .env');
+      }
+      const response = await axios.get(playersUrl, {
+        params: {
+          teamsId: teamsId,
+          seasons: seasons,
+        },
+        headers: {
+          'x-rapidapi-key': apiKey,
+          'x-rapidapi-host': apiHost,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
